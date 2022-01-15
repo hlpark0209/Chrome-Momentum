0569c8e96660f6af896d72f54293fa994b89b649
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-16 01:05:27 +0900

    Update that bring savedname and paintWelcome function  create

--- script.js
+++ script.js
@@ -6,19 +6,33 @@ const loginInput = document.querySelector('.login-form input');
 const link = document.querySelector('a');
 // string만 포함된 변수 or 중요한 정보를 담지않는 변수는 대문자료 표기
 // const HIDDEN_CLASSNAME = 'hidden';
+// const USERNAMEKEY = 'username';
 
-
+//username의 값을 받아오고 submit함
 function onLoginSubmit(e){
     //form의 default기능 제거
     e.preventDefault();
     loginForm.classList.add('hidden');
     const username = loginInput.value;
-    welcome.innerText = `Welcome ! ${username};`;
-    welcome.classList.remove('hidden');
-    console.log(welcome);
-
-
+    // username 저장
+    localStorage.setItem( "username", username);
+    paintWelcome(username);
+    
 }
-//username의 값을 받아오고 submit함
-loginForm.addEventListener('submit', onLoginSubmit);
 
+// 중복되는 function 생성
+function paintWelcome (savedUsername){
+    welcome.innerText = `Welcome ! ${savedUsername}`;
+    welcome.classList.remove('hidden');
+}
+
+// localstorage에 username 저장
+const savedUsername = localStorage.getItem("username");
+console.log(savedUsername);
+
+if( savedUsername === null ){
+    loginForm.classList.remove('hidden');
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else{
+    paintWelcome(savedUsername);
+}
