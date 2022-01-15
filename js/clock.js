930e9bbce9552b37b1009cebd85827679e1e5394
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-16 02:02:05 +0900

    Update that to string and padStart function

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,15 @@
 'use strict';
 const clock = document.querySelector('.clock');
 
-
+//string으로 변환후 sec는 00처리
 function getDate(){
     const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const min = String(date.getMinutes()).padStart(2, "0");
+    const sec = String(date.getSeconds()).padStart(2, "0");
+
     clock.innerText =
-    (`${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`);
-    
+    `${hours}:${min}:${sec}`;
 };
 
 getDate();
