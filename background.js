94f09feda78fedb137c410b8428a3d99bb270a98
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-18 00:07:49 +0900

    update

--- background.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-
-const randomBackground = ["background_01.jpg", "background_02.jpg"];
-console.log(randomBackground);
-
-// Math.floor(Math.random() * quotes.length);
--- js/background.js
+++ js/background.js
@@ -19,4 +19,4 @@ const randomBg = images[Math.floor(Math.random() * images.length)];
 // img 태크 생성 및 body에 prepend
 const createBgImg = document.createElement('img');
 createBgImg.src = `img/${randomBg}`;
-document.body.prepend(createBgImg); 
+document.body.append(createBgImg); 
